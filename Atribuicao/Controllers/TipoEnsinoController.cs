8b39e78dd172be6e04cf5c4795a8d91f127e9164
José.Eface <estudodrive@hotmail.com>
2018-09-14 14:06:07 -0300

    Adicionado visão tipo de ensino e um controller Escola

---
+++ Atribuicao/Controllers/TipoEnsinoController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Atribuicao.Controllers
+{
+    public class TipoEnsinoController : Controller
+    {
+        // GET: TipoEnsino
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
